c73562f0c97c738cba3479e0abf5eae527b02995
Anthony Navarrete <92348276+steven09n@users.noreply.github.com>
2026-06-01 20:50:39 -0600

    1
    1

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // Asegura que las rutas a los archivos sean relativas (ideal para GitHub Pages)
-  base: './',
-});
